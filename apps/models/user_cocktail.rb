0d97fccb8f273948eab5f7cf0ef69e8e0a3fa66f
gozmeister <samgorick.sg@gmail.com>
2020-04-14 17:03:29 -0500

    run file changes

--- apps/models/user_cocktail.rb
+++ apps/models/user_cocktail.rb
@@ -2,6 +2,10 @@ class UserCocktail < ActiveRecord::Base
   belongs_to :user 
   belongs_to :cocktail
 
-  
+  # def initialize(user, cocktail, rating=nil)
+  #   @user_id = user.id
+  #   @cocktail_id = cocktail.id
+  #   @rating = rating
+  # end
 
 end
