83dcf253028bd21e3cf61dedaaf2d15e75b63af1
gozmeister <samgorick.sg@gmail.com>
2020-04-14 14:08:00 -0500

    tidied up user methods

--- apps/models/user.rb
+++ apps/models/user.rb
@@ -7,5 +7,38 @@ class User < ActiveRecord::Base
     puts "#{index +1}. #{cocktail.name}"
     end
   end
+  
+  def retrieve_user_favorites
+  
+    puts "Your saved cocktails are:"
+    self.print_saved_cocktails
+  end
+  
+  def add_new_favorite
+  
+  puts "Enter the name of the cocktail you want to add:"
+  cocktail = gets.chomp
+  found_cocktail = Cocktail.search_cocktails(cocktail)
+  
+    if self.cocktails.include? found_cocktail.first
+      puts "This cocktail is already in your favorites. Your saved cocktails are:"
+      self.print_saved_cocktails
+  
+    else
+      self.cocktails << found_cocktail.first
+      puts "#{found_cocktail.first.name} added to your favorites. Your saved cocktails are now:"
+      self.print_saved_cocktails
+    end
+  end
+  
+  def delete_favorite
+    retrieve_user_favorites
+    puts "Enter the name of the cocktail that you want to remove:"
+    cocktail_to_delete = gets.chomp
+    found_cocktail = Cocktail.search_cocktails(cocktail_to_delete).first
+    self.cocktails = self.cocktails.reject { |cocktail| cocktail == found_cocktail}
+    puts "Your saved cocktails are now:"
+    self.print_saved_cocktails
+  end
 
 end
