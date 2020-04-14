bd0727a0cc8dba86dc21267fa262c75a23d71916
gozmeister <samgorick.sg@gmail.com>
2020-04-14 09:32:26 -0500

    added search functionality

--- apps/models/cocktail.rb
+++ apps/models/cocktail.rb
@@ -4,5 +4,9 @@ class Cocktail < ActiveRecord::Base
   has_many :user_cocktails
   has_many :users, through: :user_cocktails
 
+  # List all the cocktails stored within the app
+  def self.retrieve_cocktail_names
+    self.all.map { |cocktail| puts cocktail.name }
+  end
 
 end
--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,12 @@
 require_relative '../config/environment'
 
+# We will need a way to identify the user - can ask for their name?
 
+puts "Let's search for a cocktail. Enter a cocktail you want to see:"
+cocktail = gets.chomp
+found_cocktail = Cocktail.where("name like ?", "#{cocktail}").first
+ingredients = found_cocktail.ingredients
 
-puts "HELLO WORLD"
+puts "We have found a matching cocktail!"
+puts "Name: #{found_cocktail.name}"
+puts "Ingredients: #{ingredients}"
