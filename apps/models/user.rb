93408d308642dbb09ce78a68f7a60f008ff14436
gozmeister <samgorick.sg@gmail.com>
2020-04-15 19:04:12 -0500

    DRYed user methods

--- apps/models/user.rb
+++ apps/models/user.rb
@@ -3,107 +3,145 @@ class User < ActiveRecord::Base
   has_many :cocktails, through: :user_cocktails
 
   def search_cocktails
+    puts ""
+    puts "- - - - - - - - - - - - -"
+    puts ""
     puts "Let's search for a cocktail. Enter a cocktail you want to see:"
     cocktail = gets.chomp
     found_cocktail_list = Cocktail.internet_cocktails(cocktail)
   
     if found_cocktail_list
-      found_cocktail_list.each do |drink|
-        puts "Name: #{drink[:name]}"
-        puts "Ingredients: #{drink[:ingredients].join(", ")}"
-        puts "Instructions: #{drink[:instructions]}"
-      end
+      self.print_search_results(found_cocktail_list)
     else
-      puts "Sorry, that search term doesn't match anything in the database."
+      puts ""
+      puts "- - - - - - - - - - - - -"
+      puts ""
+      puts "Sorry, that cocktail could not be found."
     end
 
     self.add_favorite(found_cocktail_list)
   end
 
   def search_ingredients
+    puts ""
+    puts "- - - - - - - - - - - - -"
+    puts ""
     puts "Let's search for cocktails with the ingredient you had in mind. Enter your ingredient:"
     ingredient = gets.chomp
     found_ingredient = Ingredient.internet_search_cocktails(ingredient)
   
     if found_ingredient
-      found_ingredient.each do |drink|
-        puts "Name: #{drink[:name]}"
-        puts "Ingredients: #{drink[:ingredients].join(", ")}"
-        puts "Instructions: #{drink[:instructions]}"
-      end
+      self.print_search_results(found_ingredient)
     else
+      puts ""
+      puts "- - - - - - - - - - - - -"
+      puts ""
       puts "Sorry, that ingredient could not be found."
     end
 
     self.add_favorite(found_ingredient)
   end
 
+  def print_search_results(search_results)
+    search_results.each do |drink|
+      puts "Name: #{drink[:name]}"
+      puts "Ingredients: #{drink[:ingredients].join(", ")}"
+      puts "Instructions: #{drink[:instructions]}"
+      puts "*************"
+    end
+  end
+
   def add_favorite(search_results)
     if search_results.length == 1
+      puts ""
+      puts "- - - - - - - - - - - "
+      puts ""
       puts "Do you want to add this cocktail your favorites? (Y/N)"
       answer = gets.chomp
       if answer == "Y"
-      search_results.each do |cocktail|
+        save_favorite(search_results)
+      end
+
+    elsif search_results.length > 1
+      puts ""
+      puts "- - - - - - - - - - - - -"
+      puts ""
+      puts "Do you want to add one of these cocktails to your favorites? (Y/N)"
+      answer = gets.chomp
+      if answer == "Y"
+        puts ""
+        puts "- - - - - - - - - - - - -"
+        puts ""
+        puts "Which cocktail do you want to add?"
+        favorite = gets.chomp
+        save_multi_favorite(search_results, favorite)
+      end
+    end
+  end
+
+  def save_favorite(search_results)
+    search_results.each do |cocktail|
+      drink = Cocktail.find_or_create_by(name: cocktail[:name], instructions: cocktail[:instructions])
+      ingredients = cocktail[:ingredients]
+      ingredients.each do |ingredient|
+        one = Ingredient.find_or_create_by(name: ingredient)
+        drink.ingredients << one
+        end
+        if self.cocktails.include? drink
+          puts ""
+          puts "- - - - - - - - - - - - -"
+          puts ""
+          puts "You've already added this drink to your favorites."
+        else
+          self.cocktails << drink
+          puts ""
+          puts "- - - - - - - - - - - - -"
+          puts ""
+          puts "#{drink.name} has been added to your favorites."
+        end
+    end
+  end
+
+  def save_multi_favorite(search_results, favorite)
+    search_results.each do |cocktail|
+      if cocktail[:name] == favorite
         drink = Cocktail.find_or_create_by(name: cocktail[:name], instructions: cocktail[:instructions])
         ingredients = cocktail[:ingredients]
         ingredients.each do |ingredient|
           one = Ingredient.find_or_create_by(name: ingredient)
           drink.ingredients << one
           end
-        self.cocktails << drink
-        puts "#{drink.name} has been added to your favorites."
-        end
-      end
-    elsif search_results.length > 1
-      puts "Do you want to add one of these cocktails to your favorites? (Y/N)"
-      answer = gets.chomp
-      if answer == "Y"
-        puts "Type in the name of the cocktail you want to save into your favorites:"
-        favorite_cocktail = gets.chomp
-        search_results.each do |cocktail|
-          if cocktail[:name] == favorite_cocktail
-            drink = Cocktail.find_or_create_by(name: cocktail[:name], instructions: cocktail[:instructions])
-            ingredients = cocktail[:ingredients]
-            ingredients.each do |ingredient|
-              one = Ingredient.find_or_create_by(name: ingredient)
-              drink.ingredients << one
-            end
+          if self.cocktails.include? drink
+            puts ""
+            puts "- - - - - - - - - - - - -"
+            puts ""
+            puts "You've already added this drink to your favorites."
+          else
             self.cocktails << drink
+            puts ""
+            puts "- - - - - - - - - - - - -"
+            puts ""
             puts "#{drink.name} has been added to your favorites."
           end
-        end
       end
     end
   end
 
-  
-
-  def print_saved_cocktails
-    self.cocktails.each_with_index do |cocktail, index|
-    puts "#{index +1}. #{cocktail.name}"
-    end
-  end
-  
   def retrieve_user_favorites
-  
+    puts ""
+    puts "- - - - - - - - - - - - -"
+    puts ""
     puts "Your saved cocktails are:"
     self.print_saved_cocktails
   end
-  
-  def add_new_favorite
-  
-  puts "Enter the name of the cocktail you want to add:"
-  cocktail = gets.chomp
-  found_cocktail = Cocktail.search_cocktails(cocktail)
-  
-    if self.cocktails.include? found_cocktail.first
-      puts "This cocktail is already in your favorites. Your saved cocktails are:"
-      self.print_saved_cocktails
-  
-    else
-      self.cocktails << found_cocktail.first
-      puts "#{found_cocktail.first.name} added to your favorites. Your saved cocktails are now:"
-      self.print_saved_cocktails
+
+  def print_saved_cocktails
+    self.cocktails.each_with_index do |cocktail, index|
+    ingredients = cocktail.ingredients.map {|ingredient| ingredient.name}.join(", ")
+    puts "#{index +1}. #{cocktail.name}"
+    puts "Ingredients: #{ingredients}"
+    puts "Instructions: #{cocktail.instructions}"
+    puts "******************"
     end
   end
   
@@ -112,8 +150,8 @@ class User < ActiveRecord::Base
     puts "Enter the name of the cocktail that you want to remove:"
     cocktail_to_delete = gets.chomp
     found_cocktail = Cocktail.search_cocktails(cocktail_to_delete).first
-    self.cocktails = self.cocktails.reject { |cocktail| cocktail == found_cocktail}
-    puts "Your saved cocktails are now:"
+    self.cocktails = self.cocktails.reject { |cocktail| cocktail.name == found_cocktail.name}
+    puts "#{found_cocktail.name} has been removed from your favorites. Your saved cocktails are now:"
     self.print_saved_cocktails
   end
 
--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,8 @@
 require_relative '../config/environment'
 
 def welcome
-  # font = TTY::Font.new(:doom)
-  # puts font.write("The Bubbly App")
+  font = TTY::Font.new(:doom)
+  puts font.write("The Cocktail App")
   puts "What is your name?"
   name = gets.chomp
   if User.where("name like ?", "%#{name}%").first
@@ -16,17 +16,25 @@ end
 
 def choice(user)
   puts <<-TEXT
+
   Please enter the number corresponding to what you want to do:
+    
   1. Search cocktails by name
+
   2. Search cocktails by ingredient
+
   3. See your favorite cocktails
-  4. Add a new cocktail to your favorites
-  5. Update a rating for one of your favorite cocktails
-  6. Delete a cocktail from your favorites
-  7. Exit the program
+
+  4. Update a rating for one of your favorite cocktails
+
+  5. Delete a cocktail from your favorites
+
+  6. Exit the program
+
   ****** Enter your choice: ******
   TEXT
   input = gets.chomp
+  puts ""
   case input.to_i
   when 1
     user.search_cocktails
@@ -37,16 +45,13 @@ def choice(user)
   when 3
     user.retrieve_user_favorites
     choice(user)
-  when 4 
-    user.add_new_favorite
-    choice(user)
-  when 5
+  when 4
     user.update_rating
     choice(user)
-  when 6
+  when 5
     user.delete_favorite
     choice(user)
-  when 7
+  when 6
     user.leave
   else
     "Sorry, I don't recognise that option."
